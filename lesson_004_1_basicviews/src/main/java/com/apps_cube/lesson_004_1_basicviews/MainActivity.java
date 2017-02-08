e8c77a323cd2c4811f9dba816af5527fdc4767b0
Evgeny Kuskov <me.kuskov@gmail.com>
2017-02-08 14:23:25 +0300

    4

---
+++ lesson_004_1_basicviews/src/main/java/com/apps_cube/lesson_004_1_basicviews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.apps_cube.lesson_004_1_basicviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
